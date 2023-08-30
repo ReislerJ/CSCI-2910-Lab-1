779c9dfe6d15d72398e5b4fe917cc5e4f5722fd9
Jasmine Reisler <REISLER@ETSU.EDU>
2023-08-30 13:46:56 -0400

    Add project files.

---
+++ Lab 1/Driver.cs
@@ -0,0 +1,24 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////
+//
+//	Project:	    Lab 1
+//	File Name:		Driver.cs
+//	Description:	Where actual game runs. Demonstrates use of other class.  
+//	Course:			
+//	Author:			Jasmine, reisler@etsu.edu, Department of Computing, East Tennessee State University
+//	Created:	    Tuesday, August 29, 2023
+//	Copyright:		Jasmine Reisler, 2023
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+ public class driver
+ {
+    public static void Main()
+    {
+        //Make an object instance of a video game insatnce. Use read and write functions to take in and seperate information.
+        //Use param video game constructor to set atributes equal to certain sections of information. 
+        //
+        
+
+
+    }
+ }
---
+++ Lab 1/VideoGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    public class VideoGame
+    {
+        //Attributes
+        /* Do these names follow correct naming standards?*/
+        private string name { get; set; }
+        private string platform { get; set; }
+        private int year { get; set; }
+
+        //Would this be an enum?
+        private string genre { get; set; }
+
+        private string publisher { get; set; }
+
+        private double naSAles { get; set; }
+        private double euSales { get; set; }
+        private double jpSales { get; set; }
+        private double otherSales { get; set; }
+        private double globalSales { get; set; }
+
+        /****Constructors****/
+        //Default Constructor - called to make a basic, default set video game.
+        public VideoGames()
+        {
+            this.genre = 
+        }
+
+        //Parameterized Constructor - Used to make a video game object with specific values upon creation.
+
+        
+        //Methods
+
+
+    }
+}
